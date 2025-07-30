e675c7c5225eb276a9ac1dc2ad6cec4a477a243c
freefiremax <67188362+freefiremax@users.noreply.github.com>
2025-07-30 20:25:07 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -18,5 +18,5 @@ AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
