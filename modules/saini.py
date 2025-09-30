b055f8832db9864f7e9f6177067a26833e8bf710
nikhilsainiop <nikhilbhaiop02@gmail.com>
2025-09-30 10:54:43 +0530

    Update saini.py

--- modules/saini.py
+++ modules/saini.py
@@ -28,16 +28,7 @@ def duration(filename):
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
     return float(result.stdout)
-
-def get_mps_and_keys(api_url):
-    response = requests.get(api_url)
-    response_json = response.json()
-    #mpd = response_json.get('MPD')
-    #keys = response_json.get('KEYS')
-    mpd = response_json.get('url')
-    keys = response_json.get('keys')
-    return mpd, keys
-   
+ 
 def exec(cmd):
         process = subprocess.run(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         output = process.stdout.decode()
