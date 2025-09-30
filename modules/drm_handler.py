2356c012f5f46aeed74e7fa403b2b4667aa2c3be
nikhilsainiop <nikhilbhaiop02@gmail.com>
2025-09-30 11:22:05 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -21,7 +21,6 @@ import saini as helper
 import globals
 from utils import progress_bar
 from vars import API_ID, API_HASH, BOT_TOKEN, OWNER, CREDIT, AUTH_USERS, TOTAL_USERS, cookies_file_path
-from vars import api_url, api_token
 
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.......,.....,
 
